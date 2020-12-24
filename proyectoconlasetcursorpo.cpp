5ced5514297c842a88fece5255db1b574ecc4497
Cesar <30559667+CesarAroon@users.noreply.github.com>
2020-12-23 22:17:39 -0500

    Add files via upload

---
+++ proyectoconlasetcursorpo.cpp
@@ -0,0 +1,45 @@
+// ConsoleApplication3.cpp : main project file.
+
+#include "stdafx.h"
+
+#include<iostream>
+#include<conio.h>
+
+using namespace System;
+using namespace std;
+
+
+
+
+void dibujacuadrado(char &k, int *x, int &y)
+
+{
+	for (int i = 1; i <= 3; i++)
+
+	{
+		Console::SetCursorPosition(*x, y + i);
+		Console::BackgroundColor = ConsoleColor::Red;
+		Console::ForegroundColor = ConsoleColor::Blue;
+		for (int j = 1; j <= 3; j++)
+			cout << k;
+		cout << endl;
+	}
+
+}
+
+int main()
+{
+
+	char c;
+	int x, y;
+	char*puntero;
+	puntero = &c;
+
+	cout << "ingrese caracter:"; cin >> c;
+	cout << "ingrese xr:"; cin >> x;
+	cout << "ingrese y:"; cin >> y;
+
+	dibujacuadrado(*puntero, &x, y);
+	_getch();
+
+}
